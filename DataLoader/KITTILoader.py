7da427e941a4448fb8ce824d67bc38e3980df2b3
Gustavo Dutra <dutragustavo75@gmail.com>
2025-12-16 15:03:52 -0300

    Everything works even my romances

--- DataLoader/KITTILoader.py
+++ DataLoader/KITTILoader.py
@@ -8,11 +8,7 @@ from utils.PinholeCamera import PinholeCamera
 
 
 class KITTILoader(object):
-    default_config = {
-        "root_path": "../test_imgs",
-        "sequence": "00",
-        "start": 0
-    }
+    default_config = {"root_path": "../test_imgs", "sequence": "00", "start": 0}
 
     def __init__(self, config={}):
         self.config = self.default_config
@@ -21,16 +17,24 @@ class KITTILoader(object):
         logging.info(self.config)
 
         if self.config["sequence"] in ["00", "01", "02"]:
-            self.cam = PinholeCamera(1241.0, 376.0, 718.8560, 718.8560, 607.1928, 185.2157)
+            self.cam = PinholeCamera(
+                1241.0, 376.0, 718.8560, 718.8560, 607.1928, 185.2157
+            )
         elif self.config["sequence"] in ["03"]:
-            self.cam = PinholeCamera(1242.0, 375.0, 721.5377, 721.5377, 609.5593, 172.854)
+            self.cam = PinholeCamera(
+                1242.0, 375.0, 721.5377, 721.5377, 609.5593, 172.854
+            )
         elif self.config["sequence"] in ["04", "05", "06", "07", "08", "09", "10"]:
-            self.cam = PinholeCamera(1226.0, 370.0, 707.0912, 707.0912, 601.8873, 183.1104)
+            self.cam = PinholeCamera(
+                1226.0, 370.0, 707.0912, 707.0912, 601.8873, 183.1104
+            )
         else:
             raise ValueError(f"Unknown sequence number: {self.config['sequence']}")
 
         # read ground truth pose
-        self.pose_path = self.config["root_path"] + "/poses/" + self.config["sequence"] + ".txt"
+        self.pose_path = (
+            self.config["root_path"] + "/poses/" + self.config["sequence"] + ".txt"
+        )
         self.gt_poses = []
         with open(self.pose_path) as f:
             lines = f.readlines()
@@ -45,15 +49,27 @@ class KITTILoader(object):
 
         # image id
         self.img_id = self.config["start"]
-        self.img_N = len(glob.glob(pathname=self.config["root_path"] + "/sequences/" \
-                                            + self.config["sequence"] + "/image_0/*.png"))
+        self.img_N = len(
+            glob.glob(
+                pathname=self.config["root_path"]
+                + "/sequences/"
+                + self.config["sequence"]
+                + "/image_0/*.png"
+            )
+        )
 
     def get_cur_pose(self):
         return self.gt_poses[self.img_id - 1]
 
     def __getitem__(self, item):
-        file_name = self.config["root_path"] + "/sequences/" + self.config["sequence"] \
-                    + "/image_0/" + str(item).zfill(6) + ".png"
+        file_name = (
+            self.config["root_path"]
+            + "/sequences/"
+            + self.config["sequence"]
+            + "/image_0/"
+            + str(item).zfill(6)
+            + ".png"
+        )
         img = cv2.imread(file_name)
         return img
 
@@ -62,8 +78,14 @@ class KITTILoader(object):
 
     def __next__(self):
         if self.img_id < self.img_N:
-            file_name = self.config["root_path"] + "/sequences/" + self.config["sequence"] \
-                        + "/image_0/" + str(self.img_id).zfill(6) + ".png"
+            file_name = (
+                self.config["root_path"]
+                + "/sequences/"
+                + self.config["sequence"]
+                + "/image_0/"
+                + str(self.img_id).zfill(6)
+                + ".png"
+            )
             img = cv2.imread(file_name)
 
             self.img_id += 1
@@ -79,8 +101,16 @@ if __name__ == "__main__":
     loader = KITTILoader()
 
     for img in tqdm(loader):
-        cv2.putText(img, "Press any key but Esc to continue, press Esc to exit", (10, 30),
-                    cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 255), 1, 8)
+        cv2.putText(
+            img,
+            "Press any key but Esc to continue, press Esc to exit",
+            (10, 30),
+            cv2.FONT_HERSHEY_PLAIN,
+            1,
+            (0, 0, 255),
+            1,
+            8,
+        )
         cv2.imshow("img", img)
         # press Esc to exit
         if cv2.waitKey() == 27:
